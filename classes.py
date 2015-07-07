c1f4363bf55094699b1b46176062b90af784ab23
Choscura <choscura@gmail.com>
2015-07-06 22:34:38 -0700

    updating classes.py 7-6-15 Seattle [0]

--- classes.py
+++ classes.py
@@ -29,29 +29,72 @@ class User:
 	Username = ""						#	username
 	email = ""							#	email address
 	def __init__(self):
+		"""
+		in production, this needs to handle user acount generation and adding users to the program logic from memory. For now, though, the idea is to start with a function that can be called to generate "test users".
+		"""
+		pass
+	def read(self):
+		pass
+	def update(self):
+		pass
+	def delete(self):
 		pass
 
 class Node:
 	"""
 	nodes are computers on the network.
 	"""
+	def read(self):
+		pass
+	def update(self):
+		pass
+	def delete(self):
+		pass
 	pass
 
 class Content:
 	"""
-	Content is the stuff people make and put up for sale.
+	Content is the stuff people make and put up for sale. 
+
+	The structure of content is this: 
 	"""	
+	def read(self):
+		pass
+	def update(self):
+		pass
+	def delete(self):
+		pass
+	pass
+
+class ConPiece:
+	"""
+	a ConPiece, or Content Piece, is the individual packet of data transferred between nodes of the network.
+	"""
 	pass
 
 class Media:
 	"""
-	Media is web stuff that presents the content for sale on the website.
+	Media is web stuff that presents the content for sale on the website. By default, it describes HTML and javascript, but this is a major security hole. Maybe some workaround with another markup will work better. But for now, the prototype should show vanilla HTML. I'll add more constraints as they are needed.
+
+	**More Research Needed**
 	"""
+	def read(self):
+		pass
+	def update(self):
+		pass
+	def delete(self):
+		pass
 	pass
 
 class Authentication:
 	"""
-	Authentication is how the network tracks who's who, has what, and has sent which, when and to whom.
+	Authentication is how the network tracks who's who, has what, and has sent which, when and to whom.  It consists of a set of keys that are recycled, and the combinations of which correlate to an interior database for record keeping. there are other parts to this, mostly based around making it difficult to predict, intercept, decipher, and use, even in long-term attacks, and so for the long term players the idea is to send them on a rat race of the same data over and over, and to change what it means every time. In old-school encryption terms, the idea is to keep anybody outside at a permanent depth of one (ref leo marks between silk and cyanide).
 	"""
+	def read(self):
+		pass
+	def update(self):
+		pass
+	def delete(self):
+		pass
 	pass
 
