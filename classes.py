ce58384e9f36d71d35fdc8dc24cf7296d179e77c
Choscura <choscura@gmail.com>
2015-07-14 13:14:47 -0700

    Fucking github isn't showing my commits goddamnit.

--- classes.py
+++ classes.py
@@ -25,9 +25,28 @@ class User:
 			Inmates are abusive users who seek to undermine the network deliberately.  This can be anything from trying to steal other user's data or content to adding backdoors to content or what have you.  The intention is to not simply treat this as an error, but to make the network actively hostile to abuse and even revenge-seeking, to the scale of the damage done (corrupting inmate data, adding backdoors back into their systems with the very data they steal through their own backdoor, recuperating financial losses or inflicting expenses upon them to undo any benefit they gain, etc).
 			Do unto others, after all.
 	"""
-	UID = "" 							#	Unique Identification. every user is unique.
-	Username = ""						#	username
-	email = ""							#	email address
+	
+	UID 				= 	"" 				#	Unique Identification. every user is unique.
+	Username 			= 	""				#	username
+	email 				= 	""				#	email address
+	
+	#User Cultural and National Identity attributes
+	Languages 			=	{}				#	never assume somebody only speaks as few languages as you do.
+	Interface_Language 	=	""				#	Still, everybody has a preferred default.
+	Time_Zone			=	"London, UK"	#	The default time zone the user sees all things in relation to <default London, UK, because it's +0 GMT>
+	Country				=	""				#
+
+	#Network attributes
+	User_Nodes			=	{}				#	set of all nodes that have historically used this user's credentials
+
+
+	#User Fiscal Attributes
+	User_Local_Currency =	""				# 	what the default unit is for earned income
+	Earned_Income 		= 	0.0				# 	How much money a user has earned
+	Accumulated_Debt	=	0.0				#	How much the user has that they still need to pay for
+	Connected_Accounts	=	{}				#	Which financial institution accounts are connected to this one
+
+	#User Functions
 	def __init__(self):
 		"""
 		in production, this needs to handle user acount generation and adding users to the program logic from memory. For now, though, the idea is to start with a function that can be called to generate "test users".
@@ -45,9 +64,15 @@ class Node:
 	nodes are computers on the network.
 	A node holds data (the content pieces, assembled into the content that the user bought or pirated, which are supplied to the network), processes input (user Authentication), and takes actions (sending and receiving content pieces, authenticating with the central server, etc). 
 	"""
-	mac_address = "" 			# all computers have one, although spoofing is a risk
-	user_credentials = "" 		# a representation of the username/pass the node proves it has
-	content = {}				# a dictionary of the content- ID and Path : key and value
+	mac_address 		=	"" 				# 	all computers have one, although spoofing is a risk
+	user_credentials 	= 	"" 				# 	a representation of the username/pass the node proves it has
+	content 			= 	{}				# 	a dictionary of the content- ID and Path : key and value
+
+	#Node internal logic
+	Filepath			=	""				#	The default directory where files are searched for from.  Should include a 'gitignore' knockoff to not include local files.
+	FUCKYOUGITHUB		= 	"Why the fuck are my git commits not showing up?"
+
+	#Functions
 	def read(self):
 		pass
 	def update(self):
@@ -85,6 +110,9 @@ class Content:
 		This stricture sounds complicated, but the idea is to implement versions on platform, and so there are two tools to update 
 
 	"""	
+	#Base Attributes
+	Content_Name 			=		""			#	Content Name
+	Content_ID				=		""			#	Content unique
 	def read(self):
 		pass
 	def update(self):
@@ -112,6 +140,7 @@ class ConPiece:
 	successful_uploads 	= 	0 				#	tracking how many times a given piece has been successfully uploaded, for money.
 	attempted_uploads 	= 	0				#	tracking how many times a given piece has been unsuccessfully uploaded, for no money.
 	network_density 	= 	0 				#	tracking how many nodes on the network are also attempting to upload said file
+	
 	#data members used only be the server
 	
 	#initialize
@@ -133,10 +162,10 @@ class Media:
 
 	**More Research Needed**
 	"""
-	Media_ID 		= ""				#	tracking ID. 
-	Media_Name 		= ""				#	a name for humans to track.
-	Media_Val 		= ""				#	the actual stuff that the media represents. 
-	Media_Type 		= ""				#	the actual type of 				
+	Media_ID 			= 	""				#	tracking ID. 
+	Media_Name 			= 	""				#	a name for humans to track.
+	Media_Val 			= 	""				#	the actual stuff that the media represents. 
+	Media_Type 			= 	""				#	the actual type of the media in question.		
 	def read(self):
 		pass
 	def update(self):
