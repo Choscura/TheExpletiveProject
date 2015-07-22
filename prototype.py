3ada878c66c7048419fe0b47e9f3f4839c7c350c
Choscura <choscura@gmail.com>
2015-07-22 14:52:23 -0700

    Making some magic. fucking some things up too. not working, but saving my place anyway. Signed-off-by: Choscura <choscura@gmail.com>

--- prototype.py
+++ prototype.py
@@ -16,11 +16,11 @@ def __init__():
 #This is hows I mathing. Kawthort.
 def mathmaker(nodes, pirates, songs, price, share, customers):		#nodes are the compters, songs are the things shared, price is cost, share is %awarded, customers is # sold
 	#sanitize data; get it all to be doubles
-	print("pirate share is: %s") %(((((pirates/nodes) * share) * customers)/songs)-(songs*price)/pirates)
+	print("pirate share is: %s spread per %r pirates" %pirates, %(((((pirates/nodes) * share) * customers)/songs)-(songs*price)/pirates)
 	
 	print("non-pirate share is: %s")	%(((((nodes-pirates) * share) * customers)/songs)/(nodes-pirates))
 	
-	print("network share is: %s")%(((songs*price)*share) * customers)
+	print("network share is: %s")%((price*share) * customers)
 	
 	print("Total sales volume")
 	print(price * customers)
